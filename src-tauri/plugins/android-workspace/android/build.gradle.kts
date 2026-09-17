6d67c5a37c73ac63f04d0ee922d17692073ed0ba
Alan <80423834+az1nn@users.noreply.github.com>
2026-09-17 12:05:41 -0300

    build(android): configure SAF workspace library

---
+++ src-tauri/plugins/android-workspace/android/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.dailycockpit.workspace"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation(project(":tauri-android"))
+}
